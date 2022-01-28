e3d56c4bd274a8e472de1fbe1c9603c9e94e1d14
Aman Kumar <87615256+amankumarjsr@users.noreply.github.com>
2022-01-28 22:31:33 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from datetime import date
+from unicodedata import name
 from urllib import request
 import requests
 from bs4 import BeautifulSoup as bs
@@ -6,6 +7,7 @@ import pandas as pd
 import datetime
 import os
 import zipfile
+import glob
 
 
 CoinName= input('Enter the coin name: ').upper()
@@ -28,6 +30,8 @@ for date in date_generated:
     x=date.strftime("%Y-%m-%d")
     date_list.append(x)
 
+file_name_list= []
+cols=['opening time', 'opening price','highest price','lowest price','closing price','volume','closing time','turnover','number of transactions','active buy volume','NA','NAN']
 
 for item in date_list:
     try:
@@ -41,10 +45,27 @@ for item in date_list:
         with zipfile.ZipFile(file_name, 'r') as zip_ref:
             zip_ref.extractall('C:/Users/rocka/Desktop/Practice python/Binance data scrapper/data')
 
+        file_name_list.append(file_name+'.csv')
+
         os.remove(file_name) 
 
     except:
         print('skipped')
         continue
+
+
+master_df= pd.DataFrame()
+
+for file in os.listdir('C:/Users/rocka/Desktop/Practice python/Binance data scrapper/data'):
+    if file.endswith('.csv'):
+        master_df= master_df.append(pd.read_csv('C:/Users/rocka/Desktop/Practice python/Binance data scrapper/data/'+file, names= cols))
+master_df.to_csv(f'{CoinName}-{duration}-master file.csv', index=False)
+
+for file in os.listdir('C:/Users/rocka/Desktop/Practice python/Binance data scrapper/data'):
+    if file.endswith('.csv'):
+        os.remove('C:/Users/rocka/Desktop/Practice python/Binance data scrapper/data/'+file)
+
+
+
 print('Data Scrapped sucessfully!!!')
 
